1df1322c3e751514b56e96737c3a58aaede6c8a5
User <User@User-PC>
2020-04-22 23:25:39 +0530

    initial commit of project

---
+++ GitDemoProject/src/com/vtz/test/Test.java
@@ -0,0 +1,9 @@
+package com.vtz.test;
+
+public class Test {
+  public static void main(String[] args) {
+
+	  Utility u = new Utility();
+	System.out.println(u.getSysDate());
+}
+}
---
+++ GitDemoProject/src/com/vtz/test/Utility.java
@@ -0,0 +1,12 @@
+package com.vtz.test;
+
+import java.util.Date;
+
+public class Utility {
+
+	
+	 public Date getSysDate() {
+		 System.out.println("Utility.getSysDate()");
+		 return new Date();
+	 }
+}
